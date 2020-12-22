ad7fa2a0ef1b930908b1d01642db38d32eabb2fe
yahsashay <akshay.akshay.ece16@itbhu.ac.in>
2020-12-22 22:42:42 +0530

    2nd lesson

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-const product = 'Socks'
+const app = Vue.createApp ({
+    data() { // ES6 Shorthand ==> data: function()
+        return {
+            product: 'Socks',
+            description: 'black'
+        }
+    }
+})
+
+
+/**
+ * const app = Vue.createApp({ OPTIONAL OBJECT })
+ * WE HAVE TO PASS AT LEAT A EMPTY OBJECT TO START AN APP
+ * doing this we create a vue instance which is the heart of our vue application
+ * which powers are whole applications
+ * 
+ * data(){
+ * 
+ * }
+ * 
+ * how that happens 
+ * under the hood vue is reactive
+ * there is  a reactivity system running under the hood
+ * automatically update without us knowing.
+ * 
+ * 
+ * 
+ */
